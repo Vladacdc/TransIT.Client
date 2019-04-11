c5e12f4149225b07627ca41b9b8890f4210486d5
Rostyslav Toch <rokarostuk@gmail.com>
2019-04-11 19:44:03 +0300

    Changed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/core/components/login/login.component';
+import { AdminGuard } from './modules/core/guards/admin.guard';
+import { LoginGuard } from './modules/core/guards/login.guard';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [LoginGuard]
   },
   {
     path: 'admin',
-    loadChildren: './modules/admin/admin.module#AdminModule'
+    loadChildren: './modules/admin/admin.module#AdminModule',
+    canActivate: [AdminGuard]
   },
   {
     path: '**',
