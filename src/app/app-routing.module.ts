1e5d1940d50ed8a0873ae2c651611f57e7080faf
Rostyslav Toch <rokarostuk@gmail.com>
2019-04-15 09:25:09 +0300

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
   },
   {
     path: 'admin',
-    loadChildren: './modules/admin/admin.module#AdminModule'
+    loadChildren: './modules/admin/admin.module#AdminModule',
+    canActivate: [AdminGuard]
   },
   {
     path: '**',
