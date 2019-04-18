51802ab60f42ebdc96c8e1f347c3cb57712a5d4d
Maksym Yaremko <maksym.yaremko@gmail.com>
2019-04-18 21:02:45 +0300

    add data

--- src/app/modules/admin/component/malfunctions/malfunc-group/malfunc-group.component.ts
+++ src/app/modules/admin/component/malfunctions/malfunc-group/malfunc-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-malfunc-group',
@@ -6,14 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./malfunc-group.component.scss']
 })
 export class MalfuncGroupComponent implements OnInit {
-
+  heroes = [
+    new Hero(1, 'Windstorm','Asap'),
+    new Hero(13, 'Bombasto','Arap'),
+    new Hero(15, 'Magneta','Arack'),
+    new Hero(20, 'Tornado','Atar')
+  ];
   constructor() { }
 
   ngOnInit() {
     $('#group').DataTable({
       language: {
         url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
-      }
+      },
+      ajax: "../../assets/data.json",
+      columns: [
+        { "data": "id" },
+        { "data": "name" },
+        { "data": "position" },
+      ]
     });
   }
 
