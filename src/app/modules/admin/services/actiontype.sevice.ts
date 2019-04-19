2a53a402926622727d2755638ea6c5b0cbf5c62f
markiyanshvets <34346633+markiyanshvets@users.noreply.github.com>
2019-04-19 14:56:33 +0300

    18 action type view (#30)
    * action-type
    
    * fix

--- src/app/modules/admin/services/actiontype.sevice.ts
+++ src/app/modules/admin/services/actiontype.sevice.ts
@@ -7,6 +7,6 @@ import { environment } from 'src/environments/environment';
     providedIn: 'root'
   })
   export class ActionTypeService extends CrudService<ActionType> {
-    protected readonly serviceUrl = `${environment.apiUrl}/action`;
+    protected readonly serviceUrl = `${environment.apiUrl}/actiontype`;
   }
   
