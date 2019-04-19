a443a067da9590b6b9f05600dcc3380109c9a1ef
Taras Kvasnytsya <taras1kvasnytsya@gmail.com>
2019-04-19 12:59:25 +0300

    add tables

--- src/app/modules/admin/component/malfunctions/malfunc-group/malfunc-group.component.ts
+++ src/app/modules/admin/component/malfunctions/malfunc-group/malfunc-group.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
 
 @Component({
   selector: 'app-malfunc-group',
@@ -7,25 +6,14 @@ import { Hero } from './hero';
   styleUrls: ['./malfunc-group.component.scss']
 })
 export class MalfuncGroupComponent implements OnInit {
-  heroes = [
-    new Hero(1, 'Windstorm','Asap'),
-    new Hero(13, 'Bombasto','Arap'),
-    new Hero(15, 'Magneta','Arack'),
-    new Hero(20, 'Tornado','Atar')
-  ];
+  
   constructor() { }
 
   ngOnInit() {
     $('#group').DataTable({
       language: {
         url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
-      },
-      ajax: "../../assets/data.json",
-      columns: [
-        { "data": "id" },
-        { "data": "name" },
-        { "data": "position" },
-      ]
+      }      
     });
   }
 
