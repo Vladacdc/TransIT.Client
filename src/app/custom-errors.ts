6ab8e11312d647556733035e93bc0f51b3b39d5a
Rostyslav Toch <RokaRostuk@gmail.com>
2019-04-25 20:17:02 +0300

    Fixed styles in app (#69)
    * Fixed styles in app
    
    * Added custom errors

--- src/app/custom-errors.ts
+++ src/app/custom-errors.ts
@@ -4,9 +4,25 @@ export const CUSTOM_ERRORS: ErrorMessage[] = [
   {
     error: 'required',
     format: requiredFormat
+  },
+  {
+    error: 'minlength',
+    format: minLengthFormat
+  },
+  {
+    error: 'maxlength',
+    format: maxLengthFormat
   }
 ];
 
 export function requiredFormat(label: string, error: any): string {
   return `Поле "${label}" є обов'язковим`;
+}
+
+export function minLengthFormat(label: string, error: any): string {
+  return `Мінімальна довжина поля - ${error.requiredLength}`;
+}
+
+export function maxLengthFormat(label: string, error: any): string {
+  return `Максимальна довжина поля - ${error.requiredLength}`;
 }
