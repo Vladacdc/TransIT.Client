97a4906d32a88b4ac9279ea263a411b8c2f179f8
Rostyslav Toch <RokaRostuk@gmail.com>
2019-05-02 15:23:50 +0300

    Fixed guard bug (#79)

--- src/app/modules/core/guards/engineer.guard.ts
+++ src/app/modules/core/guards/engineer.guard.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-import { AuthenticationService } from '../services/authentication.service';
-import { CanActivate } from '@angular/router';
+import { LoginGuard } from './login.guard';
+import { Role } from '../models/role/role';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EngineerGuard implements CanActivate {
-  constructor(private service: AuthenticationService) {}
-
-  canActivate(): boolean {
-    return this.service.getRole() === 'ENGINEER';
-  }
+export class EngineerGuard extends LoginGuard {
+  protected expectedRole: Role = 'ENGINEER';
 }
