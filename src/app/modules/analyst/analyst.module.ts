86eff76302972af4579ed68a0c27078338e5cbd1
Iryna Panchuk <rusa1234527@gmail.com>
2019-05-07 17:25:23 +0300

    add navbar to analyst (#93)
    * add navbar to analyst
    
    * add issue

--- src/app/modules/analyst/analyst.module.ts
+++ src/app/modules/analyst/analyst.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AnalystComponent } from './component/analyst/analyst.component';
 import { AnalystRoutingModule } from './analyst-routing.module';
 import { IssueAnalystComponent } from './component/issue-analyst/issue-analyst.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [AnalystComponent, IssueAnalystComponent],
   exports: [],
-  imports: [CommonModule, AnalystRoutingModule]
+  imports: [CommonModule, AnalystRoutingModule, CoreModule]
 })
 export class AnalystModule {}
