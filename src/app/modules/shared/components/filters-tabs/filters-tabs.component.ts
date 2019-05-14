9689763900f56e20ab415e160c756f23d5e49126
Iryna Panchuk <rusa1234527@gmail.com>
2019-05-14 17:26:55 +0300

    all work reallygit add .git add . (#117)

--- src/app/modules/shared/components/filters-tabs/filters-tabs.component.ts
+++ src/app/modules/shared/components/filters-tabs/filters-tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { toDate } from '@angular/common/src/i18n/format_date';
 
 @Component({
   selector: 'app-filters-tabs',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters-tabs.component.scss']
 })
 export class FiltersTabsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+    $('#startDate').datepicker({
+      uiLibrary: 'bootstrap4',
+      //  minDate: today,
+      maxDate: function() {
+        return $('#endDate').val();
+      }
+    });
+    $('#endDate').datepicker({
+      uiLibrary: 'bootstrap4',
+      minDate: function() {
+        return $('#startDate').val();
+      }
+    });
   }
-
 }
