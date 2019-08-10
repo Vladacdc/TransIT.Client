ff6976cefba191753e238c80b77d1c39f9aae87b
Vladyslav Rohov <38084885+Vladacdc@users.noreply.github.com>
2019-08-10 13:59:50 +0300

    Reworked supplier edit and create (#30)
    * Reworked edit-supplier
    
    * Reworked supplier create & edit

--- src/app/modules/shared/components/supplier/edit-supplier/edit-supplier.component.ts
+++ src/app/modules/shared/components/supplier/edit-supplier/edit-supplier.component.ts
@@ -7,6 +7,7 @@ import { SupplierService } from '../../../services/supplier.service';
 import { ToastrService } from 'ngx-toastr';
 import { CurrencyService } from '../../../services/currency.service';
 import { CountryService } from '../../../services/country.service';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-edit-supplier',
@@ -17,38 +18,36 @@ export class EditSupplierComponent implements OnInit {
   selectedSupplier: Supplier;
   countries: Array<Country>;
   currencies: Array<Currency>;
-  country: Country;
-  currency: Currency;
   supplierForm: FormGroup;
-  @ViewChild('close') closeDiv: ElementRef;  
+
+  @ViewChild('close') closeDiv: ElementRef;
   @Output() updateSupplier = new EventEmitter<Supplier>();
   @Input()
   set supplier(supplier: Supplier) {
     if (!supplier) {
       return;
     }
-    this.selectedSupplier=supplier;
-    supplier = new Supplier(supplier);
-    this.supplierForm.patchValue({...supplier, currency: supplier.currency ? supplier.currency.id : null, country: supplier.country ? supplier.country.id : null});
-  };
+    this.selectedSupplier = new Supplier(supplier);
+    this.supplierForm.patchValue(this.selectedSupplier);
+  }
 
   constructor(
     private currencyService: CurrencyService,
     private countryService: CountryService,
-    private formBuilder: FormBuilder, 
-    private service: SupplierService, 
+    private formBuilder: FormBuilder,
+    private service: SupplierService,
     private toast: ToastrService
-    ) {}
-  
-   ngOnInit() {
-        this.supplierForm = this.formBuilder.group({
+  ) {}
+
+  ngOnInit() {
+    this.supplierForm = this.formBuilder.group({
       id: [''],
       name: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(30)])),
-      fullName: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(500)])),
+      fullName: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(60)])),
       edrpou: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(14)])),
       country: [''],
-      currency: [''],
-   });    
+      currency: ['']
+    });
     this.countryService.getEntities().subscribe(data => {
       this.countries = data;
     });
@@ -64,45 +63,31 @@ export class EditSupplierComponent implements OnInit {
     return this.currencies.map(e => e.fullName);
   }
 
-
   updateData() {
     if (this.supplierForm.invalid) {
       return;
     }
-    this.closeDiv.nativeElement.click();
     const form = this.supplierForm.value;
-
-    let currentCurrency = null;
-    this.currencies.forEach(element => {
-      if(element.id == form.currency){
-        currentCurrency = element;
-      }
-      
-    });
-
-    let currentCountry = null;
-    this.countries.forEach(element => {
-      if(element.id == form.country){
-        currentCountry = element;
-      }
-      
-    });
-
     const supplier: Supplier = {
       id: form.id as number,
       name: form.name as string,
       fullName: form.fullName as string,
       edrpou: form.edrpou as string,
-      country: currentCountry as Country,
-      currency: currentCurrency as Currency
+      currency: form.currency as Currency,
+      country: form.country as Country
     };
-    this.service.updateEntity(supplier).subscribe(_ => 
-      {
+
+    this.service.updateEntity(supplier).subscribe(
+      _ => {
         this.toast.success('', 'Постачальника оновлено');
-        this.updateSupplier.next(supplier)
+        this.updateSupplier.next(supplier);
       },
-      error => this.toast.error('Помилка редагування'));
+      error => this.toast.error('Помилка редагування')
+    );
     this.closeDiv.nativeElement.click();
   }
-}
 
+  resetForm() {
+    this.supplierForm.patchValue(this.selectedSupplier);
+  }
+}
