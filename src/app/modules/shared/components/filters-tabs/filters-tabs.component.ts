0e95f1083d85d584b35cbd497094a2cab41bf180
Vitalii Maksymiv <pbbbbc@gmail.com>
2019-08-27 00:24:12 +0300

    ÐвÑdisable malf

--- src/app/modules/shared/components/filters-tabs/filters-tabs.component.ts
+++ src/app/modules/shared/components/filters-tabs/filters-tabs.component.ts
@@ -12,13 +12,11 @@ import { MalfunctionSubgroupService } from '../../services/malfunction-subgroup.
 import { VehicleTypeService } from '../../services/vehicle-type.service';
 import { Location } from '../../models/location';
 import { LocationService } from '../../services/location.service';
-import { MAT_DATE_LOCALE } from '@angular/material';
 
 @Component({
   selector: 'app-filters-tabs',
   templateUrl: './filters-tabs.component.html',
-  styleUrls: ['./filters-tabs.component.scss'],
-  providers: [{provide: MAT_DATE_LOCALE, useValue: 'en-GB'}]
+  styleUrls: ['./filters-tabs.component.scss']
 })
 export class FiltersTabsComponent implements OnInit {
   vehicleTypeList: VehicleType[] = [];
@@ -172,6 +170,8 @@ export class FiltersTabsComponent implements OnInit {
     this.selectedMalfunctionSubGroup = undefined;
     this.selectedMalfunction = undefined;
     this.selectedLocation = undefined;
+    this.malfunctionDisabled = true;
+    this.malfunctionSubGroupDisabled = true;
     this.selectFilter();
   }
 }
