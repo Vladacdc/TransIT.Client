a8fdd644a6ffa4a041c3d884d9073a6fafcfc5e0
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2019-08-27 00:27:11 +0300

    add common dummy logic into main part-in component

--- src/app/modules/shared/components/dictionaries/parts-in/parts-in.component.ts
+++ src/app/modules/shared/components/dictionaries/parts-in/parts-in.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { EntitiesDataSource } from '../../../data-sources/entities-data-sourse';
 import { PartIn } from '../../../models/part-in';
 import { PartsInService } from '../../../services/parts-in.service';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { SpinnerService } from 'src/app/modules/core/services/spinner.service';
+import { ToastrService } from 'ngx-toastr';
+import { tap, map, catchError } from 'rxjs/operators';
+import { Currency } from '../../../models/currency';
+import { CurrencyService } from '../../../services/currency.service';
 
 @Component({
   selector: 'app-parts-in',
@@ -22,11 +27,52 @@ export class PartsInComponent implements OnInit {
   ];
 
   dataSource: EntitiesDataSource<PartIn>;
+  currencies: Currency[];
 
-  constructor(private partsInService: PartsInService) {
+  constructor(
+    private partsInService: PartsInService,
+    private currencyService: CurrencyService,
+    private spinnerService: SpinnerService,
+    private toastrService: ToastrService) {
   }
 
   ngOnInit() {
     this.dataSource = new EntitiesDataSource<PartIn>(this.partsInService);
+    // Loading data
+    this.currencyService.getEntities().subscribe(data => {
+      this.currencies = data;
+    });
+  }
+
+  private decorateRequest(request: Observable<PartIn>): Observable<PartIn> {
+    return request.pipe(
+      tap(() => this.spinnerService.show()),
+      map(data => data),
+      tap(() => this.spinnerService.hide())
+    );
+  }
+
+  addEntity(entity: PartIn) {
+    this.decorateRequest(this.partsInService.addEntity(entity))
+      .subscribe(
+        ok => this.toastrService.success(),
+        error => this.toastrService.error('TODO: Error adding entity')
+      );
+  }
+
+  editEntity(entity: PartIn) {
+    this.decorateRequest(this.partsInService.updateEntity(entity))
+      .subscribe(
+        ok => this.toastrService.success('TODO: Successfully edited'),
+        error => this.toastrService.error('TODO: General Error')
+      );
+  }
+
+  deleteEntity(entity: PartIn) {
+    this.decorateRequest(this.partsInService.deleteEntity(entity.id))
+      .subscribe(
+        ok => this.toastrService.success('TODO: Succesfully deleted'),
+        error => this.toastrService.error('TODO: General Error')
+      );
   }
 }
