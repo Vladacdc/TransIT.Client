4aafe343fb2fccb82c910f4d6c6909029b760a5f
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2019-08-27 17:59:58 +0300

    Make working ng template

--- src/app/modules/shared/components/dictionaries/parts-in/part-in-actions/part-in-actions.component.ts
+++ src/app/modules/shared/components/dictionaries/parts-in/part-in-actions/part-in-actions.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { PartIn } from 'src/app/modules/shared/models/part-in';
 
 @Component({
   selector: 'app-part-in-actions',
   templateUrl: './part-in-actions.component.html',
   styleUrls: ['./part-in-actions.component.scss']
 })
-export class PartInActionsComponent implements OnInit {
+export class PartInActionsComponent implements OnInit, AfterViewInit {
+
+  @Input() partIn: PartIn;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.partIn);
   }
 
+  ngAfterViewInit() {
+    debugger;
+    console.log(this.partIn);
+  }
+
+  removeItem() {
+    console.log('hi angular');
+  }
 }
