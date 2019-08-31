b8c53356dd606c5af6ef15fffe4e2c2029e28947
Vlad <38084885+Vladacdc@users.noreply.github.com>
2019-08-31 12:34:46 +0300

    Improved Breadcrumb and more

--- src/app/modules/localization/localization.module.ts
+++ src/app/modules/localization/localization.module.ts
@@ -10,7 +10,8 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     return new MultiTranslateHttpLoader(httpClient, [
         {prefix: './assets/translate/core/', suffix: '.json'},
         {prefix: './assets/translate/', suffix: '.json'},
-        {prefix: './assets/translate/shared/filter-tab/', suffix: '.json'}       
+        {prefix: './assets/translate/routing/', suffix: '.json'},
+        {prefix: './assets/translate/shared/filter-tab/', suffix: '.json'}
     ]);
   }
 
@@ -33,7 +34,7 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
 export class LocalizationModule {
     constructor(
       public translate: TranslateService,
-      private _adapter: DateAdapter<any>) {
+      private adapter: DateAdapter<any>) {
         this.ConfigureTranslation();
     }
     private ConfigureTranslation() {
@@ -41,7 +42,7 @@ export class LocalizationModule {
         this.translate.setDefaultLang('uk');
         this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
           localStorage.setItem('language', event.lang);
-          this._adapter.setLocale(event.lang);
+          this.adapter.setLocale(event.lang);
         });
 
         let language = this.translate.getDefaultLang();
