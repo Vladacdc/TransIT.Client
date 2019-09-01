82a06ace26d2dec7563e56f3d7ac95119815d518
BohdanBuhrii <bobuhrii@gmail.com>
2019-09-01 15:00:34 +0300

    Cleaning

--- src/app/modules/analyst/components/charts/charts.component.ts
+++ src/app/modules/analyst/components/charts/charts.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartsModule } from 'ng2-charts';
 import { VehicleTypeService } from 'src/app/modules/shared/services/vehicle-type.service';
 import { StatisticsService } from 'src/app/modules/shared/services/statistics.service';
 import { MalfunctionService } from 'src/app/modules/shared/services/malfunction.service';
@@ -12,7 +11,7 @@ import { Malfunction } from 'src/app/modules/shared/models/malfunction';
   styleUrls: ['./charts.component.scss']
 })
 export class ChartsComponent implements OnInit {
-  //private barChartReady: boolean;
+
   private pieChartReady: boolean;
 
   private malfunctions: Malfunction[];
@@ -26,24 +25,11 @@ export class ChartsComponent implements OnInit {
     private vehicleTypeService: VehicleTypeService,
     private malfunctionService: MalfunctionService,
     private statisticsService: StatisticsService) {
-      //this.barChartReady=false;
       this.pieChartReady=false;
       this.pieChartLabels=[];
       this.pieChartData=[];
       this.pieChartType='pie';
   }
-  
-  /*public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];*/
 
   ngOnInit() {
     this.malfunctionService.getEntities().subscribe(data => {
