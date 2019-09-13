61c46668907ce05324463e77517ad45d20330083
BohdanBuhrii <bobuhrii@gmail.com>
2019-09-13 17:08:57 +0300

    Rework states and actions tables

--- src/app/modules/admin/component/action-state-dictionary/action-dictionary/action-dictionary.component.ts
+++ src/app/modules/admin/component/action-state-dictionary/action-dictionary/action-dictionary.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActionType } from 'src/app/modules/shared/models/action-type';
+import { MatFspTableComponent } from 'src/app/modules/shared/components/tables/mat-fsp-table/mat-fsp-table.component';
+import { EntitiesDataSource } from 'src/app/modules/shared/data-sources/entities-data-sourse';
 import { ActionTypeService } from 'src/app/modules/shared/services/action-type.service';
-import { DatatableSettings } from 'src/app/modules/shared/helpers/datatable-settings';
-
-declare const $;
 
 @Component({
   selector: 'app-action-dictionary',
@@ -13,71 +10,24 @@ declare const $;
   styleUrls: ['./action-dictionary.component.scss']
 })
 export class ActionDictionaryComponent implements OnInit {
-  actions: ActionType[] = [];
-  tableAction: any;
-  selectedAction: ActionType;
-  tost: ToastrService;
+  columnDefinitions: string[] = [
+    'name'
+  ];
+  columnNames: string[] = [
+    'Назва'
+  ]
 
-  constructor(private actionService: ActionTypeService, private router: Router, private toast: ToastrService) {}
+  @ViewChild('table') table: MatFspTableComponent;
 
-  private readonly tableConfig = new DatatableSettings({
-    columns: [
-      { title: 'Назва', data: 'name', defaultContent: '' },
-      { data: 'id', visible: false },
-      { title: 'Дії⠀', orderable: false }
-    ],
-    ajax: this.ajaxCallback.bind(this),
-    columnDefs: [
-      {
-        targets: -1,
-        data: null,
-        defaultContent: `<button class="first btn" data-toggle="modal" data-target="#editAction"><i class="fas fa-edit"></i></button>
-         <button class="second btn" data-toggle="modal" data-target="#deleteAction"><i class="fas fas fa-trash-alt"></i></button>`
-      }
-    ],
-    language: {
-      url: 'assets/language.json'
-    }
-  });
+  dataSource: EntitiesDataSource<ActionType>;
+
+  constructor(private actionTypeService: ActionTypeService) { }
 
   ngOnInit() {
-    this.tableAction = $('#action-table').DataTable(this.tableConfig);
-    $('#action-table tbody').on('click', '.first', this.selectFirstItem(this));
-    $('#action-table tbody').on('click', '.second', this.selectSecondItem(this));
+    this.dataSource = new EntitiesDataSource<ActionType>(this.actionTypeService);
   }
 
-  private ajaxCallback(dataTablesParameters: any, callback): void {
-    this.actionService.getFilteredEntities(dataTablesParameters).subscribe(x => {
-      callback(x);
-    });
-  }
-
-  selectFirstItem(component: any) {
-    return function() {
-      const data = component.tableAction.row($(this).parents('tr')).data();
-      component.selectedAction = data;
-    };
-  }
-
-  selectSecondItem(component: any) {
-    return function() {
-      const data = component.tableAction.row($(this).parents('tr')).data();
-      component.selectedAction = data;
-    };
-  }
-
-  addAction(action: ActionType) {
-    this.actions.push(action);
-    this.tableAction.draw();
-  }
-
-  deleteAction(action: ActionType) {
-    this.actions = this.actions.filter(v => v.id !== action.id);
-    this.tableAction.draw();
-  }
-
-  editAction(action: ActionType) {
-    this.actions[this.actions.findIndex(i => i.id === action.id)] = action;
-    this.tableAction.draw();
+  refreshTable() {
+    this.table.loadEntitiesPage();
   }
 }
